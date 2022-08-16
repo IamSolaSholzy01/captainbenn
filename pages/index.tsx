7d24475885f876386ae315eefebbf8a9c9920b8e
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-08-16 09:46:07 +0100

    Initializing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,29 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import Layout from "../layouts/Layout";
+import Image from "next/image";
 
 const Home: NextPage<{ title: string }> = ({ title }) => {
   return (
     <Layout title={title}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        {/*<div className={styles.grid}>*/}
-        {/*  <a href="https://nextjs.org/docs" className={styles.card}>*/}
-        {/*    <h2>Documentation &rarr;</h2>*/}
-        {/*    <p>Find in-depth information about Next.js features and API.</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a href="https://nextjs.org/learn" className={styles.card}>*/}
-        {/*    <h2>Learn &rarr;</h2>*/}
-        {/*    <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a*/}
-        {/*    href="https://github.com/vercel/next.js/tree/canary/examples"*/}
-        {/*    className={styles.card}*/}
-        {/*  >*/}
-        {/*    <h2>Examples &rarr;</h2>*/}
-        {/*    <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a*/}
-        {/*    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-        {/*    className={styles.card}*/}
-        {/*  >*/}
-        {/*    <h2>Deploy &rarr;</h2>*/}
-        {/*    <p>*/}
-        {/*      Instantly deploy your Next.js site to a public URL with Vercel.*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
-        {/*</div>*/}
-      </div>
-      <section>Join our foundation</section>
+      <section className={styles.heroSection}>
+        <div className={styles.hero}>
+          <Image alt={"logo"} src={"/images/hero.png"} layout={"fill"} />
+        </div>
+      </section>
+      <section>
+        <h3>Ready to Rumble</h3>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit.
+        </span>
+        <div>
+          <button>Get Started</button>
+          <div>Watch video</div>
+        </div>
+      </section>
     </Layout>
   );
 };
