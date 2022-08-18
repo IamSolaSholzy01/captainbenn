e8b03cf1fbfd8350532b6e7d205d25dc442b50a8
Powerisinschool <toolulopeolagunju@gmail.com>
2022-08-18 12:42:24 +0100

    contact page mobile view

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -152,10 +152,20 @@ const MobileHeader: ({ title }: { title: string }) => JSX.Element = ({
 }) => {
   const [showMobileNav, setMobileNav] = React.useState(false);
   return (
-    <>
+    <header className={styles.mobile}>
       <MobileNav show={showMobileNav} setShow={setMobileNav} title={title} />
-      <span onClick={() => setMobileNav(true)}>burger</span>
-    </>
+      <div className={styles.headerNav}>
+        {title === "Home" || <Logo />}
+        <span className={styles.burger} onClick={() => setMobileNav(true)}>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="4" y1="6" x2="20" y2="6" />
+            <line x1="4" y1="12" x2="20" y2="12" />
+            <line x1="4" y1="18" x2="20" y2="18" />
+          </svg>
+        </span>
+      </div>
+    </header>
   );
 };
 
